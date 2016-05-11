bb23b6e467515c8922ba0b4109fd4ebdc9c168b9
Erik Lundmark <mail@elundmark.se>
2016-05-11 17:20:38 +0200

    v0.2.7 Return to the old icon

--- convert_youtube_embeds_to_image_links.meta.js
+++ convert_youtube_embeds_to_image_links.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Convert Youtube Embeds to Image Links
 // @namespace	 http://elundmark.se/code/
-// @version      0.2.4
-// @date         2015-10-21
+// @version      0.2.7
+// @date         2016-05-11
 // @downloadURL  http://f.3r1k.se/js/convert_youtube_embeds_to_image_links/convert_youtube_embeds_to_image_links.user.js
 // ==/UserScript==
